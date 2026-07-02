0a95ebbbd847fc9208e27edd5eb4f808e6b7914b
Ray Stallard <rinmynth@gmail.com>
2026-07-02 12:03:01 -0700

    did Messages.java and SoccerApp.java

--- src/main/java/week01/part04_constants/SoccerApp.java
+++ src/main/java/week01/part04_constants/SoccerApp.java
@@ -18,6 +18,7 @@ public class SoccerApp {
     // - Call the helper methods below in the correct order.
     public static void runMatch() {
         // Your code here
+        System.out.println(Messages.TITLE);
     }
 
     // TODO: Print EXACTLY one line using Messages.TITLE
